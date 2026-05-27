be75a93fd010036e7461b3edf14239aecf27cd19
jakegithubpage <jmcclure.789@outlook.com>
2026-05-26 21:30:56 -0400

    Started thinking and setting up solver source and header

--- SRC/cc_solver.c
+++ SRC/cc_solver.c
@@ -1,2 +1,24 @@
-/* Ustilize methods of mesh or nodal analysis to
+/* Utilize methods of mesh or nodal analysis OR voltage/curr divider to
  pre solve circuits and cook solution and statistics for user when they finally finish solving and enter answer to compare*/
+
+
+
+
+#include "CktEngine.h"
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+static void cc_calculator(CB_Ckt out*) {
+unsigned summer;
+unsigned UnNode;
+    //One unknown node/Two total - Difficulty base is 0
+    if (out->nodeCount == 1u) {
+        for (unsigned i = 0; out->componentCount; ++i) {
+
+        }
+    }
+}
+//Come back to rethink better
